7bbde54d3941a098f7d995d74c60bfcec2bc8004
youxkei <youxkei@gmail.com>
2019-02-17 05:24:49 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+chrome.commands.onCommand.addListener(() => {
+  chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true
+  }, async tabs => {
+    const tab = tabs[0];
+
+    const textArea = document.createElement('textarea');
+    textArea.value = `[${tab.title}](${tab.url})`;
+    document.body.appendChild(textArea);
+
+    textArea.select();
+    document.execCommand('copy');
+
+    document.body.removeChild(textArea);
+  });
+});

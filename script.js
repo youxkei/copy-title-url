fb85f394ee5bdc77b3b48dd4509afa5be2a27f21
youxkei <youxkei@gmail.com>
2019-06-06 19:50:54 +0900

    Handle [] in title

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ chrome.commands.onCommand.addListener(() => {
     const tab = tabs[0];
 
     const textArea = document.createElement('textarea');
-    textArea.value = `[${tab.title}](${tab.url})`;
+    textArea.value = `[${tab.title.replace(/\[/g, '［').replace(/]/g, '］')}](${tab.url})`;
     document.body.appendChild(textArea);
 
     textArea.select();
